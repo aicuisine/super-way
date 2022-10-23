7b66c06de835011f8adfeafc5e24093b7d8df3f5
Hamees Ahmed Khan <hameesa.khan@gmail.com>
2022-10-23 13:07:30 +0500

    Initial decomposition of template

---
+++ resources/views/public/partials/experience.blade.php
@@ -0,0 +1,46 @@
+
+<section class="ftco-section ftco-counter img ftco-no-pt" id="section-counter">
+  <div class="container">
+    <div class="row d-md-flex">
+      <div class="col-md-9">
+        <div class="row d-md-flex align-items-center">
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="18">0</strong>
+                <span>Years of Experienced</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="100">0</strong>
+                <span>Menus/Dish</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="50">0</strong>
+                <span>Staffs</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="15000">0</strong>
+                <span>Happy Customers</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 text-center text-md-left">
+        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+      </div>
+    </div>
+  </div>
+</section>
